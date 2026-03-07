4f40c09dc352e31ac8681fc0999ba38cafd64c5e
kamury <whitewatercolor@gmail.com>
2026-03-07 12:14:20 +0300

    fix logs

--- HelloFlask/__init__.py
+++ HelloFlask/__init__.py
@@ -8,7 +8,12 @@ from . import config, db, models
 app = Flask(__name__)
 app.config.from_object(config)
 
+
 # Инициализируем MySQL с этим приложением
 db.init_app(app)
+#настраиваем логгирование
+setup_logger(app)
+
+app.logger.info('Тестовое сообщение лога')
 
 from .routes import main, api
